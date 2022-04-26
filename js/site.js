50ab2cfd93c54f5128a305a59f1ceaf37b3c7111
P4L4DIN-BL4CKCOD3 <80567073+P4L4DIN-BL4CKCOD3@users.noreply.github.com>
2022-04-26 13:32:30 +0200

    Initial commit
    Created project - still need styling

--- js/site.js
+++ js/site.js
@@ -1,4 +1,35 @@
-//Call Hello Word
-function HelloWorld(){
-    alert("Hello World");
+//The first thing we need to do is to "get" the string from the page.
+//We can do this with the "DOM" or Document Object Model in js. 
+//CONTROLLER FUNCTION
+function getValue(){
+    
+    document.getElementById("alert").classList.add("invisible");    
+    let userString = document.getElementById("userString").value;    
+    let revString = reverseString(userString);    
+    displayString(revString);
+}
+
+//The second thing we want to do is to reverse the string.
+//LOGIC FUNCTION
+function reverseString(userString){
+    
+    //Decleare the "revString" variable into an arry.
+    let revString = [];    
+    //Reverse the decleared array with a "for loop".
+    for (let index = userString.length -1; index >= 0; index--) {
+        revString += userString[index];
+        
+    }
+    return revString;
+}
+
+//The third thing we want to do is to display the results to the user.
+//VIEW FUNCTION
+function displayString(_revstring){
+    
+    //Write the output of "revString" to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${_revstring}`;    
+    //Turn on the "alert box" to diplay the output of "revString"
+    document.getElementById("alert").classList.remove("invisible");
+
 }
